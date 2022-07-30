ac2a434d65594bf0290fc6b6e672560d89914534
Andrey Avdeev <a3avdeev@gmail.com>
2022-07-31 01:47:37 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,50 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+function bodyColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+startBtn.style.width = '50px';
+startBtn.style.height = '30px';
+startBtn.style.border = '1px solid black';
+startBtn.style.borderRadius = '2px';
+startBtn.style.boxShadow = '0 5px 10px grey';
+startBtn.style.marginLeft = '40%';
+startBtn.style.marginTop = '10%';
+
+stopBtn.style.width = '50px';
+stopBtn.style.height = '30px';
+stopBtn.style.border = '1px solid grey';
+stopBtn.style.borderRadius = '2px';
+stopBtn.style.boxShadow = '0 0 0 transparent';
+stopBtn.setAttribute('disabled', 'disabled');
+
+startBtn.addEventListener('click', startChangeColor);
+stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+  intervId = setInterval(bodyColor, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+  startBtn.style.border = '1px solid grey';
+  startBtn.style.boxShadow = '0 0 0 transparent';
+  stopBtn.removeAttribute('disabled', 'disabled');
+  stopBtn.style.border = '1px solid black';
+  stopBtn.style.borderRadius = '2px';
+  stopBtn.style.boxShadow = '0 5px 10px grey';
+}
+
+function stopChangeColor() {
+  clearInterval(intervId);
+  stopBtn.setAttribute('disabled', 'disabled');
+  stopBtn.style.border = '1px solid grey';
+  stopBtn.style.boxShadow = '0 0 0 transparent';
+  startBtn.removeAttribute('disabled', 'disabled');
+  startBtn.style.border = '1px solid black';
+  startBtn.style.borderRadius = '2px';
+  startBtn.style.boxShadow = '0 5px 10px grey';
+}
